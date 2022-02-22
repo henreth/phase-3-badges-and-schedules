0802b236da29594537bd09cb0240550d92a03d26
Henry Miller <mail.henrym+git@gmail.com>
2022-02-22 15:08:51 -0500

    commit tree 5242a49c353cc3cd674c99e389dbeeb30b1cf4cb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(namelist)
+    namelist.map{|name| badge_maker(name)}
+end 
+
+def assign_rooms(namelist)
+    namelist.map.with_index(1){|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+end 
+
+def printer(namelist)
+    batch_badge_creator(namelist).each{|badge| puts badge}
+    assign_rooms(namelist).each{|msg| puts msg}
+end
